8fdab645923aaa228363b8f9e3ac39d45621894d
Blake Thompson <blthomps3@gmail.com>
2026-03-23 09:19:14 -0500

    added basic starting JS

---
+++ assets/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+infobox = document.getElementById('info');
+about = document.getElementById('about');
+contact = document.getElementById('contact');
+
+about.addEventListener('click', function() {
+
+});
+
+contact.addEventListener('click', function() {
+    
+});
