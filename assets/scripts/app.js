9ffc903d3ae43f92ad6e778eb382676d67dc6798
Blake Thompson <blthomps3@gmail.com>
2026-03-23 13:43:42 -0500

    Did some minor changes to other parts of the document, added the beginnings of a footer

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,9 +5,9 @@ about = document.getElementById('about');
 contact = document.getElementById('contact');
 
 about.addEventListener('click', function() {
-
+    info.innerText = "About";
 });
 
 contact.addEventListener('click', function() {
-    
+    info.innerText = "I can be reached at blthomps3@gmail.com.";
 });
